9455e5e9ca605839ceaa77c1657cc64aa8883e90
Babu Reddy <babureddym17@gamil.com>
2022-11-29 15:17:52 +0530

    Predicate interface using lambda

--- Java8-basics/src/com/basics/funtionalInterface/PredicateDemo.java
+++ Java8-basics/src/com/basics/funtionalInterface/PredicateDemo.java
@@ -1,22 +1,36 @@
 package com.basics.funtionalInterface;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
 
-public class PredicateDemo implements Predicate<Integer>{
+public class PredicateDemo /*implements Predicate<Integer>*/{
 	
 	
 
 	public static void main(String[] args) {
-		Predicate<Integer> predicate = new PredicateDemo();
-		System.out.println(predicate.test(20));
+		/*
+		 * Predicate<Integer> predicate = new PredicateDemo();
+		 * System.out.println(predicate.test(20));
+		 */
+		
+		/*
+		 * Predicate<Integer> predicate = (t) -> { if( t%2 == 0) { return true; } return
+		 * false; }; System.out.println(predicate.test(3));
+		 */
+		
+		/*
+		 * Predicate<Integer> predicate = (t) -> t%2 == 0;
+		 * System.out.println(predicate.test(6));
+		 */
+		List<Integer> numberList = Arrays.asList(12,13,17,10);
+		
+		numberList.stream().filter((t) -> t%2 == 0).forEach(t -> System.out.println("The o/p is: " +t));
 	}
 
-	@Override
-	public boolean test(Integer t) {
-		if(t % 2 == 0) {
-			return true;
-		}
-		return false;
-	}
+	/*
+	 * @Override public boolean test(Integer t) { if(t % 2 == 0) { return true; }
+	 * return false; }
+	 */
 
 }
